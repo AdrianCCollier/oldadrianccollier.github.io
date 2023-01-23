34afd00040722eacff3a48951f5fb24e781d2ced
Adrian <AdrianCCollier@gmail.com>
2023-01-23 15:18:43 -0700

    React Search bar Integration - Unfinished

--- frontend/visualapp/src/App.js
+++ frontend/visualapp/src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import {React} from 'react';
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 import Footer from './components/footer';
+import SearchBar from './components/SearchBar'
+// import data from "./SearchData.json"
+import BlackBox from './components/BlackBox';
 import Navbar from "./components/navbar";
 
 function App() {
- 
+  
   return (
-    <div className="app-container">
+    <div className="App">
       <Navbar />
-      <h1>Hi</h1>
+      <SearchBar placeholder="Search a song" />
+      <h1>This is a test </h1>
+      {/* <BlackBox /> */}
       <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
