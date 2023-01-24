c4fd82779a4802cfee21c8a6452667a68be12b3a
Adrian <AdrianCCollier@gmail.com>
2023-01-24 07:22:08 -0700

    gallery.js

--- website/src/App.js
+++ website/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/navbar'
+import Gallery from './components/gallery'
+import Footer from './components/footer'
+// import SearchBar from './components/SearchBar'
+// // import data from "./SearchData.json"
+// import BlackBox from './components/BlackBox'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      {/* <SearchBar placeholder="Search a song" /> */}
+      <h1>Hi Hello Hey </h1>
+      <gallery />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ website/src/components/gallery.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../gallery.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function gallery() {
+  return (
+    <div className="gallery center">
+      <div className="first"></div>
+      <div className="second"></div>
+      <div className="third"></div>
+    </div>
+  )
+}
