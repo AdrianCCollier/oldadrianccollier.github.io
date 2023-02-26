cfce3085f2b3de5d03f4a7f4283cc560bac11fc7
Adrian <AdrianCCollier@gmail.com>
2023-02-26 12:45:14 -0700

    Created Selection board and first ship

--- battleShipPage/battleshipGame.js
+++ battleShipPage/battleshipGame.js
@@ -1,9 +1,22 @@
+const player1SelectionBoard = document.getElementById("player1-selection-board");
+
 const player1Board = document.getElementById("player1-board");
 const player2Board = document.getElementById("player2-board");
 
+const gridSize = 10;
 // Here, we use a simple nested for loop to create our 10x10 grid and append it to our game board
-for(let i = 0; i < 10; i++) {
-    for(let j = 0; j < 10; j++) {
+for(let i = 0; i < gridSize; i++) {
+    for(let j = 0; j < gridSize; j++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        player1SelectionBoard.appendChild(cell);
+    }
+}
+
+
+// Here, we use a simple nested for loop to create our 10x10 grid and append it to our game board
+for(let i = 0; i < gridSize; i++) {
+    for(let j = 0; j < gridSize; j++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
         player1Board.appendChild(cell);
