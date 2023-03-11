f880e6ac6835d691540b751e9e4ae50d6b4ae86c
Adrian <AdrianCCollier@gmail.com>
2023-03-11 12:00:37 -0700

    beginning

---
+++ calculator/app.js
@@ -0,0 +1,37 @@
+const display = document.getElementById('display');
+const AC = document.getElementById('AC');
+const plusButton = document.getElementById('plus');
+const twoButton = document.getElementById('2')
+const equalButton = document.getElementById('=');
+const minusButton = document.getElementById('-');
+const onButton = document.getElementById('Empty');
+
+let total = 0;
+let array = [];
+AC.onclick = function() {
+display.textContent = total;
+}
+
+plusButton.onclick = function() {
+    plus = +
+array.push(plus)
+}
+
+equalButton.onclick = function() {
+    display.textContent = total;
+}
+
+onButton.onclick = function() {
+    window.alert('you will die in 7 days');
+}
+
+twoButton.onclick = function() {
+    two = 2;
+array.push(two)
+}
+
+console.log(array);
+
+
+
+
