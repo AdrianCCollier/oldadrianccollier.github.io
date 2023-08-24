3d83ce0e4dd979de2e5b0ca7251bfcaa03b4e6bb
Adrian <AdrianCCollier@gmail.com>
2023-08-24 01:39:27 -0600

    new

--- MovieProject/routes/movies.js
+++ MovieProject/routes/movies.js
@@ -2,25 +2,27 @@ const express = require('express')
 const router = express.Router()
 const Movie = require('../models/movie')
 
-// Get all movies
+// GET route
 router.get('/', async (req, res) => {
   try {
-    const movies = await Movie.find()
+    const query = {}
+    if (req.query.minRating) {
+      query.rating = { $gte: Number(req.query.minRating) }
+    }
+    const movies = await Movie.find(query).sort({ rating: 1 })
     res.send(movies)
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
 })
-// Get one movie
-router.get('/:id', getMovie, (req, res) => {
-  res.json(res.movie)
-})
 
-// Create one movie
-router.post('/', async (req, res) => {
+// POST route
+router.post('/movie', async (req, res) => {
   const movie = new Movie({
     name: req.body.name,
     genre: req.body.genre,
+    releaseDate: req.body.releaseDate,
+    rating: req.body.rating,
   })
 
   try {
@@ -31,48 +33,4 @@ router.post('/', async (req, res) => {
   }
 })
 
-// Update one movie
-// put updates all movie info at once, patch only updates specified input
-router.patch('/:id', getMovie, async (req, res) => {
-  if (req.body.name != null) {
-    res.movie.name = req.body.name
-  }
-  if (req.body.genre != null) {
-    res.movie.genre = req.body.genre
-  }
-
-  try {
-    const updatedMovie = await res.movie.save()
-    res.json(updatedMovie)
-  } catch (error) {
-    res.status(400).json({ message: err.message })
-  }
-})
-
-// Delete one movie
-router.delete('/:id', getMovie, async (req, res) => {
-  try {
-    await Movie.deleteOne({ _id: res.movie._id })
-    res.json({ message: 'Deleted Movie' })
-  } catch (err) {
-    res.status(500).json({ message: err.message })
-  }
-})
-
-// Middleware
-async function getMovie(req, res, next) {
-  let movie
-  try {
-    movie = await Movie.findById(req.params.id)
-    if (movie == null) {
-      return res.status(404).json({ message: 'Cannot find movie' })
-    }
-  } catch (error) {
-    return res.status(500).json({ message: error.message })
-  }
-
-  res.movie = movie
-  next()
-}
-
-module.exports = router
+module.exports = router;
--- MovieProject/server.js
+++ MovieProject/server.js
@@ -1,22 +1,53 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const PORT = 3000;
+const Movie = require('./models/movie');
+require('dotenv').config();
+
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
 })
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
 db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+db.once('open', () => console.log('Successfully connected to Database'));
 
 // setting up routes
-app.use(express.json())
+app.use(express.json());
+
+// properly parse form data
+app.use(express.urlencoded({extended: true }));
+
+// set up EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 const moviesRouter = require('./routes/movies')
 
-app.use('/movies', moviesRouter)
+app.use('/movies', moviesRouter);
+
+// serve simple html form at path /upload to allow entry of a new document into mongodb collection using HTTP post request
+app.get('/upload', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/list', async (req, res) => {
+  try {
+    const movies = await Movie.find();
+    res.render('list', { movies: movies });
+  } catch(error) {
+    console.error(error);
+    res.status(500).send('Error: Unable to fetch movies.');
+  }
+});
+
+app.get('/query', (req, res) => {
+  res.render('query');
+});
 
 // listen to server
-app.listen(3000, () => console.log('Listening on Server: 3000'))
+app.listen(PORT, () => console.log(`Listening on Port: ${PORT}`));
